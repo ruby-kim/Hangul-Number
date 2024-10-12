e51c688a29e5d8fe97a3e3f3e40c1fa5199ef76f
ruby-kim <dev.rubykim@gmail.com>
2024-10-12 11:43:20 +0900

    refactor: update parsing func simply

--- hangul_num/format.py
+++ hangul_num/format.py
@@ -1,65 +1,27 @@
 from change_list import changeList
 
 
-def get_num_unit_indices(text, unit_dict):
-    """Get starting indices for units in text."""
-    words = text.split(" ")
-    flag_idx = [
-        sum(len(words[j]) + 1 for j in range(i)) for i in range(len(words))
-    ]
-    indices = []
-    for i, word in enumerate(words):
-        for unit_list in unit_dict.values():
-            if any(val in word for val in unit_list):
-                indices.append(flag_idx[i] + word.index(unit_list[0]))
-    return indices
+def parse_korean_number(text, num_list):
+    """한글 숫자를 아라비아 숫자로 변환"""
+    unit_values = {k: v for k, v in num_list if v >= 10}
+    digit_values = {k: v for k, v in num_list if v < 10}
 
-
-def parse_num_sections(text, indices):
-    """Identify number sections based on indices."""
-    sections = []
-    for idx in indices:
-        start = next((i for i in range(idx, -1, -1) if text[i] == " "), -1) + 1
-        sections.append((start, idx))
-    return sections
-
-
-def get_converted_num_list(text, num_dict):
-    """Return converted number list from text."""
-    return [num_dict.get(num, num) for num in text]
-
-
-def calc_elements(word_list, base_num, unit_num):
-    """Calculate number based on base and unit num."""
-    num_list = []
-    for word in word_list:
-        result, tmp = 0, 0
-        for num in map(lambda w: unit_num.get(w, base_num.get(w, 0)), word):
-            if num and tmp and num > tmp:
-                result += tmp * num
-                tmp = 0
-            else:
-                tmp += num
-        num_list.append(result + tmp)
-    return num_list
-
-
-def convert_text(text, change_list, is_cardinal=True):
-    """Main function to convert text to numerical representation."""
-    unit_dict = change_list.card_unit if is_cardinal else change_list.ord_unit
-
-    indices = get_num_unit_indices(text, unit_dict)
-    sections = parse_num_sections(text, indices)
-    word_lists = [text[start:end] for start, end in sections]
-
-    num_base, num_unit = (
-        {k: v for k, v in change_list.card_num[:9]},
-        {k: v for k, v in change_list.card_num[9:]},
-    )
-    result = calc_elements(word_lists, num_base, num_unit)
-
-    return {text[s:e]: num for (s, e), num in zip(sections, result)}
-
+    result = 0
+    current = 0
+    temp_word = ""
+    
+    for char in text:
+        temp_word += char  # 한 글자씩 누적
+        if temp_word in digit_values:  # 단일 숫자 매칭
+            current = digit_values[temp_word]
+            temp_word = ""
+        elif temp_word in unit_values:  # 자릿수 단위 매칭
+            current = max(current, 1) * unit_values[temp_word]
+            result += current
+            current = 0
+            temp_word = ""
+    result += current
+    return result
 
 def hangul_num(text):
     """TODO"""
